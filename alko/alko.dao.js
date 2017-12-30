c9795eae2c17b78607e92e9ac44114c7c4b0a754
aphixus <nils.stenholm@gmail.com>
2017-12-30 23:08:01 +0200

    v1 seems to be working

--- alko/alko.dao.js
+++ alko/alko.dao.js
@@ -3,60 +3,182 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 
 let alko = {};
-const FILE_DOWNLOAD_TIMEOUT = 3000;
+let cachedData;
+const FILE_DOWNLOAD_TIMEOUT = 10000;
+const CATALOG_PATH = './alko-catalogs/';
 
 alko._getUrl = function(date){
   // date format 27.12.2017
   console.log('Creating url with date', date);
   return `https://www.alko.fi/INTERSHOP/static/WFS/Alko-OnlineShop-Site/-/Alko-OnlineShop/fi_FI/Alkon%20Hinnasto%20Tekstitiedostona/alkon-hinnasto-tekstitiedostona${date}.xls`
-  return 'https://api.github.com/users/github';
-}
+};
 
-alko._readResponse = function(body){
+alko._readResponse = function(xlsFile){
   // console.log( body);
-  const workSheetsFromBuffer = xlsx.parse(body);
+  const workSheetsFromBuffer = xlsx.parse(xlsFile);
   return workSheetsFromBuffer;
-}
+};
 
-alko.getCatalog = function(){
+const createDateStr = () => {
   const date = new Date();
   const days = date.getHours() <= 8 ? date.getDate() - 1 : date.getDate();
-  const dateStr = `${days}.${date.getMonth()+1}.${date.getFullYear()}`;
-  fetch(alko._getUrl(dateStr))
-    // .then(res => res.body)
-    // .then(res => {console.log(res);})
+  return `${days}.${date.getMonth()+1}.${date.getFullYear()}`;
+};
+
+const getCatalogPath = (dateStr) => {
+  return `${CATALOG_PATH}alko-catalog-${dateStr}.xls`;
+};
+
+const createAlkoCatalogStream = () => {
+  const path = getCatalogPath(createDateStr());
+  return {
+    stream: fs.createWriteStream(path),
+    path
+  };
+};
+
+const readCatalog = (dateStr) => {
+  const path = getCatalogPath(dateStr);
+  console.log("Checking if cached file exists at ", path);
+  return new Promise((res, rej) => {
+    // if (fs.existsSync(path)) {
+    //   console.log(fs.existsSync(path));
+    //   const foo = xlsx.parse(fs.readFileSync(path));
+    //   console.log(foo);
+    //   return res(foo);
+    // }
+
+    const url = alko._getUrl(dateStr);
+    console.log("Did not find cached catalog. Querying from Alko with url", url);
+    return res(fetchCatalog(url));
+    // fs.readFile(path, 'utf8', function (err, data) {
+    //   if (err) {
+    //     console.log("Did not find cached catalog. Querying from Alko");
+    //     const url = alko._getUrl(path);
+    //     return res(fetchCatalog(url));
+    //   }
+    //   return res(data);
+    // });
+  });
+};
+
+const fetchCatalog = (url) => {
+  return fetch(url)
     .then((response) => new Promise((res, rej) => {
-        // console.log(response);
-        // if (response.status >= 400) {
-            // throw new Error("Bad response from server");
-        // }
-        const dest = fs.createWriteStream('./alko-catalog.xls');
+        if (response.status >= 400) {
+            throw new Error("Bad response from server");
+        }
+        const { stream, path } = createAlkoCatalogStream();
         let timer;
-        response.body.pipe(dest)
+        response.body.pipe(stream)
           .on('open', () => {
             timer = setTimeout(() => {
-              stream.close()
-              rej({reason: 'Timed out downloading file', meta: {url}})
+              stream.close();
+              rej({reason: 'Timed out downloading file', meta: {url}});
             }, FILE_DOWNLOAD_TIMEOUT)
           })
           .on('error', (error) => {
-            clearTimeout(timer)
-            rej({reason: 'Unable to download file', meta: {url, error}})
+            clearTimeout(timer);
+            rej({reason: 'Unable to download file', meta: {url, error}});
           })
           .on('finish', () => {
-            clearTimeout(timer)
-            res('./alko-catalog.xls')
+            clearTimeout(timer);
+            res(path);
           });
     }))
-    .then((buffer) => {
-        const parsedResponse = alko._readResponse(buffer)
-        // console.log(parsedResponse);
-        parsedResponse
-        .forEach((entry) => {
-          const gg = entry.data[4];
-          console.log(gg);
+    .then((path) => {
+       return alko._readResponse(path)
+    });
+};
+
+const alkoCatalogToJson = (rawData, dataFormat) => {
+  const { startIndex, columns } = dataFormat;
+  const delCount = startIndex || 0;
+  const data = rawData[0].data;
+  data.splice(0, delCount);
+
+  return data.map((row, index) => {
+    let record = {};
+    columns.forEach((columnData) => {
+      record[columnData.key] = row[columnData.index];
+    });
+    return record;
+  });
+};
+
+alko.getCatalog = function(){
+  const dateStr = createDateStr();
+  if(cachedData != null){
+    return new Promise((res, rej) => {
+      res(cachedData);
+    });
+  }
+  // const url = alko._getUrl(dateStr);
+  return readCatalog(dateStr)
+    // fetch(url)
+    // .then((response) => new Promise((res, rej) => {
+    //     if (response.status >= 400) {
+    //         throw new Error("Bad response from server");
+    //     }
+    //     const { stream, path } = createAlkoCatalogStream();
+    //     let timer;
+    //     response.body.pipe(stream)
+    //       .on('open', () => {
+    //         timer = setTimeout(() => {
+    //           stream.close()
+    //           rej({reason: 'Timed out downloading file', meta: {url}})
+    //         }, FILE_DOWNLOAD_TIMEOUT)
+    //       })
+    //       .on('error', (error) => {
+    //         clearTimeout(timer)
+    //         rej({reason: 'Unable to download file', meta: {url, error}})
+    //       })
+    //       .on('finish', () => {
+    //         clearTimeout(timer)
+    //         res(path);
+    //       });
+    // }))
+    .then((parsedResponse) => {
+        // const parsedResponse = alko._readResponse(path)
+        // parsedResponse
+        // .forEach((entry) => {
+        //   const gg = entry.data[4];
+        //   console.log(gg);
+        // });
+        return alkoCatalogToJson(parsedResponse, {
+          startIndex: 4,
+          columns: [
+            { key: 'number', index: 0 },
+            { key: 'name', index: 1 },
+            { key: 'producer', index: 2 },
+            { key: 'bottle_size', index: 3 },
+            { key: 'price', index: 4 },
+            { key: 'price_per_liter', index: 5 },
+            { key: 'is_new', index: 6 },
+            { key: 'pricing_ordering_code', index: 7 },
+            { key: 'type', index: 8 },
+            { key: 'special_group', index: 9 },
+            { key: 'beer_type', index: 10 },
+            { key: 'country', index: 11 },
+            { key: 'region', index: 12 },
+            { key: 'vintage', index: 13 },
+            { key: 'labelling_info', index: 14 },
+            { key: 'additional_info', index: 15 },
+            { key: 'grape', index: 16 },
+            { key: 'desc', index: 17 },
+            { key: 'packaging_type', index: 18 },
+            { key: 'closing_type', index: 19 },
+            { key: 'alcohol_percent', index: 20 },
+            { key: 'acids_grams_per_liter', index: 21 },
+            { key: 'sugar_grams_per_liter', index: 22 },
+            { key: 'energy_kcal_per_100ml', index: 26 },
+            { key: 'selection', index: 27 },
+          ]
         });
-        return parsedResponse;
+    })
+    .then((parsedJson) => {
+      cachedData = parsedJson;
+      return parsedJson;
     })
     .catch((error) => {
       console.error('Found an error in the response', error);
--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var _ = require('lodash');
+
 var dao = require('../alko/alko.dao');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  dao.getCatalog();
-  res.send('respond with a resource');
+router.get('/v1/products', function(req, res, next) {
+  dao.getCatalog().then((data) => {
+    res.set('Content-Type', 'application/json');
+    res.send(data);
+  });
+});
+
+router.get('/v1/products/:productId', function(req, res, next) {
+  dao.getCatalog().then((data) => {
+    res.set('Content-Type', 'application/json');
+    console.log(req.params);
+    const productId = req.params.productId;
+    console.log('Finding product with id', productId);
+    const product = _.find(data, { number: productId });
+
+    console.log('Found product', product);
+    res.send(product);
+  });
 });
 
 module.exports = router;
